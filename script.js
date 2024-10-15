5967a5358dd1d8da838709218b2f8d88bb0f0b9f
Pedroviskill <96587429+Pedroviskill@users.noreply.github.com>
2024-10-15 17:27:25 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+ const form = document.getElementById('form');
+ form.addEventListener('submit',handleSubmit)
+
+ const inputValue = document.getElementById('value-real');
+ const selectedCurrency = document.getElementById('currency');
+ const result = document.getElementById('result');
+ let valueConverted=0;
+
+ function handleSubmit(e){
+    e.preventDefault();
+    if (!inputValue.value || inputValue.value < 0){
+        alert ('Informe um valor');
+        return;
+    } else if (! selectedCurrency.value) {
+        alert('escolha uma moeda');
+        return;
+    }
+
+converter();
+
+ };
+
+ function converter() {
+    if (selectedCurrency.value === "eur") {
+      valueConverted = inputValue.value / 5.5;
+      result.innerHTML = valueFormater("pt-BR", "EUR");
+  
+      animateResult();
+    } else if (selectedCurrency.value === "dol") {
+      valueConverted = inputValue.value / 5.1;
+      result.innerHTML = valueFormater("en-US", "USD");
+  
+      animateResult();
+    }
+  
+    inputValue.value = "";
+    selectedCurrency.value = "";
+  }
+  function valueFormater (Locale, currency) {
+    const value = valueConverted.toLocaleString(Locale, {
+        style: 'currency',
+        currency: currency
+    });
+    return value;
+}
+
+ function animateResult() {
+    return result.animate(
+      [{ transform: "translateY(-150px)" }, { transform: "translateY(0px)" }],
+      { duration: 500 }
+    );
+  }
